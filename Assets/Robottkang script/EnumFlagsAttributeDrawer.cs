1d34ecd9877a37a61aa6ccbb7407b532da2146a9
robottkang <robottkang39@gmail.com>
2022-05-24 21:05:45 +0900

    Feat : enum값 중복 선택 Editor script 추가
    인스펙터에서 enum의 값을 중복 선택할 수 있게 해주는 Editor script(EnumFlagsAttribute) 추가
    
    추가, [EnumFlags]을 붙여 enum 값을 중복 선택 가능

---
+++ Assets/Robottkang script/EnumFlagsAttributeDrawer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
+public class EnumFlagsAttributeDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        property.intValue = EditorGUI.MaskField(position, label, property.intValue, property.enumNames);
+    }
+}

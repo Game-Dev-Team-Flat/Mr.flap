85122111e4b168c6e3d1290e45126e68caabeb7b
robottkang <robottkang39@gmail.com>
2022-08-10 22:34:43 +0900

    Chore : 부모의 scale의 영향을 받지 않음

--- Assets/Robottkang script/Editor/DetectTargetEditor.cs
+++ Assets/Robottkang script/Editor/DetectTargetEditor.cs
@@ -17,10 +17,10 @@ public class DetectTargetEditor : Editor
             detectTarget.detectingArea = (GameObject)EditorGUILayout.ObjectField("Detecting Area", detectTarget.detectingArea, typeof(GameObject), true);
 
             detectTarget.detectingArea.transform.localScale =
-                Vector3.up * detectTarget.detectionDistance * Mathf.Cos(seta)
-                + new Vector3(1, 0, 1) * detectTarget.detectionDistance * Mathf.Sin(seta) * 2;
+                Vector3.up * detectTarget.detectionDistance * Mathf.Cos(seta) / detectTarget.transform.localScale.y
+                + new Vector3(1f / detectTarget.transform.localScale.x, 0f, 1f / detectTarget.transform.localScale.z) * detectTarget.detectionDistance * Mathf.Sin(seta) * 2;
             detectTarget.detectingArea.transform.GetChild(1).localScale =
-                new Vector3(1, 1, 0) + Vector3.forward / detectTarget.detectingArea.transform.localScale.y * detectTarget.detectionDistance * 2 * (1 - Mathf.Cos(seta));
+                new Vector3(1f, 1f, 0f) + Vector3.forward / detectTarget.detectingArea.transform.localScale.y * detectTarget.detectionDistance * 2 * (1 - Mathf.Cos(seta)) / detectTarget.transform.localScale.z;
         }
     }
 }

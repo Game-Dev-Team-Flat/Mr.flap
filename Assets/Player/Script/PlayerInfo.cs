10a9b734c968320d9a7ebb1f930969ee04c8fe5c
robottkang <robottkang39@gmail.com>
2022-10-19 15:28:06 +0900

    Feat : 피격 판정 구현

--- Assets/Player/Script/PlayerInfo.cs
+++ Assets/Player/Script/PlayerInfo.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerInfo : EntityInfo
 {
+    public bool isHit = false;
+    [SerializeField]
+    private int hitCount = 0;
     [SerializeField]
     private Transform m_eyesOfObject;
     [SerializeField]
@@ -14,4 +17,15 @@ public class PlayerInfo : EntityInfo
     public KeyCode reloadKey => m_reloadKey;
     public KeyCode changeShotModeKey => m_changeShotModeKey;
     public Transform eyesOfObject => m_eyesOfObject;
+
+    protected override void CalculateDamage()
+    {
+        if(takenDamage > 0f)
+        {
+            isHit = true;
+            hitCount++;
+        }
+
+        base.CalculateDamage();
+    }
 }
